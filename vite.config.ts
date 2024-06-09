e4236d70587eab67f44f3ceaad1842d8c3da3cba
Misa Liu <misaliu@misaliu.top>
2024-06-10 03:45:24 +0800

    chore(Vite): Add alias

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import * as path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
+});
