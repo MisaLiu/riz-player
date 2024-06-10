2c1eacec9f0cd65868c51344811f20edc0cdba95
Misa Liu <misaliu@misaliu.top>
2024-06-11 01:04:16 +0800

    feat(audio/clock): Audio clock init

--- src/audio/clock.ts
+++ src/audio/clock.ts
@@ -16,11 +16,18 @@ export class GameAudioClock {
     this._ctx = audioCtx;
     this._ticker = ticker;
 
-    this.calcTick();
-    this._ticker.add(() => this.calcTick());
+    this._initTicker();
   }
 
-  private calcTick() {
+  private _initTicker() {
+    if (this._ctx.state === 'running') this._ticker.add(() => this._calcTick());
+    else this._ctx.addEventListener('statechange', () => {
+      if (this._ctx.state !== 'running') return;
+      this._ticker.add(() => this._calcTick());
+    });
+  }
+
+  private _calcTick() {
     const realTime = performance.now() / 1000;
     const delta = realTime - this._ctx.currentTime;
 
