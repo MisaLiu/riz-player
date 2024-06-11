fc66a5b00b77a52244584da9d28f02eb7c28a879
Misa Liu <misaliu@misaliu.top>
2024-06-12 00:59:20 +0800

    chore(Vite): Define package info as global constant

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import * as path from 'path';
 import { defineConfig } from 'vite';
+import PackageInfo from './package.json';
 
 export default defineConfig({
+  define: {
+    __APP_NAME__: JSON.stringify(PackageInfo.name),
+    __APP_AUTHOR__: JSON.stringify(PackageInfo.author.name),
+    __APP_VERSION__: JSON.stringify(PackageInfo.version),
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
